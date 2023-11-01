21e586a9b0f7a2fd7aeedff2fb5504908555c059
Dealer03 <crazydealer2003@protonmail.com>
2023-11-01 00:55:20 -0400

    Adds final styling and citations

--- world-countries/src/app/app.component.ts
+++ world-countries/src/app/app.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'World Countries';
+  source= 'World Bank. (2023). World Development Indicators 2023. ';
+  reference = 'Data provided by:';
+  link = 'https://databank.worldbank.org/source/world-development-indicators';
   selectedCountryName: string = ''; // Initialize as an empty string
   
   onCountrySelected(countryName: string) {
